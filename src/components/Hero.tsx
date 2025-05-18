0f172ee16f7d11e9f37ad89410bf1fa79aa63ccc
Shakeer Samanthapudi <159039058+Samanthapudi-Shakeer@users.noreply.github.com>
2025-05-18 22:45:26 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion';
 import { ArrowDown } from 'lucide-react';
-import image_me from './img.png';
+import {image_me} from './img.png';
 
 export default function Hero() {
   return (
